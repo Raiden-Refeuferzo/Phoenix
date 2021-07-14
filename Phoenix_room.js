54ed965e104c7f27792f1da1aab3e869a3faf9a2
Raiden-Refuerzo <79947126+Raiden-Refeuferzo@users.noreply.github.com>
2021-07-14 15:16:59 -0700

    Add files via upload

--- Phoenix_room.js
+++ Phoenix_room.js
@@ -11,12 +11,30 @@ var firebaseConfig = {
     };
     // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
-  
+    user_name=localStorage.getItem(Username)
+    document.getElementById(user_name).innerHTML="user_name"
+    function add_room()
+    {
+          room_name=document.getElementById("room_name").value;
+          firebase.database().ref("/").child(room_name).update({
+                purpose:"adding room name"
+          });
+          localStorage.setItem("room_name",room_name);
+          window.location="Phoenix_message.js";
+    }
 
 function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
        Room_names = childKey;
       //Start code
-
+      console.log("room name-"+Room_names);
+      row="<div class='room_name' id="+Room_names+"onclick='redirectToRoomName(this.id)'>#"+Room_names+"</div><hr>";
+      document.getElementById("output").innerHTML+=row;
       //End code
       });});}
 getData();
+function redirectToRoomName(name)
+{
+      console.log(name);
+      localStorage.setItem("room_name", name);
+      window.location="Phoenix_message.js";
+}
